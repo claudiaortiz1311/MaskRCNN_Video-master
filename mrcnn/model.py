a0a773d9ff74f810c85388d34231e3417118372b
Claudia Ortiz <claudiaortiz1311@gmail.com>
2024-10-24 16:11:21 -0300

    Update model.py
    El argumento workers era válido en versiones anteriores de TensorFlow/Keras para indicar el número de procesos de trabajadores para generar los datos en paralelo. Sin embargo, en las versiones más recientes, esta funcionalidad ha cambiado o sido modificada, lo que provoca este error cuando intentas utilizar workers.

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -2356,7 +2356,6 @@ class MaskRCNN():
             callbacks=callbacks,
             validation_data=val_generator,
             validation_steps=self.config.VALIDATION_STEPS,
-            workers=workers,
             use_multiprocessing=True,
         )
         self.epoch = max(self.epoch, epochs)
